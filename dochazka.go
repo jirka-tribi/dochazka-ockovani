647d88617d26bccd0bbb736524d1b90a58f95a71
Jiří Tribula <jiri.tribula@avast.com>
2021-04-11 21:32:09 +0200

    wip

--- dochazka.go
+++ dochazka.go
@@ -1,30 +1,138 @@
 package main
 
 import (
+	"bytes"
+	"embed"
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
+	"image"
+	"image/png"
+	"strconv"
+	"time"
 )
 
-func main() {
-	var inTE, outTE *walk.TextEdit
+//go:embed ico.png
+var icon embed.FS
 
-	MainWindow{
-		Title:   "Dochazka",
-		MinSize: Size{200, 200},
-		Layout:  VBox{},
-		Children: []Widget{
-			HSplitter{
-				Children: []Widget{
-					TextEdit{AssignTo: &inTE},
-					TextEdit{AssignTo: &outTE, ReadOnly: true},
+func getIcon() image.Image {
+	dataBytes, _ := icon.ReadFile("ico.png")
+	dataReader := bytes.NewReader(dataBytes)
+	imageIcon, _ := png.Decode(dataReader)
+	return imageIcon
+}
+
+type Months struct {
+	Id   int
+	Name string
+}
+
+func monthNames() []*Months {
+	return []*Months{
+		{1, "Leden"},
+		{2, "Únor"},
+		{3, "Březen"},
+		{4, "Duben"},
+		{5, "Květen"},
+		{6, "Červen"},
+		{7, "Červenec"},
+		{8, "Srpen"},
+		{9, "Září"},
+		{10, "Říjen"},
+		{11, "Listopad"},
+		{12, "Prosinec"},
+	}
+}
+
+type MyMainWindow struct {
+	*walk.MainWindow
+}
+
+func main() {
+	mainWindow := new(MyMainWindow)
+
+	var monthName *walk.ComboBox
+	var year *walk.LineEdit
+
+	now := time.Now()
+	nowMonth := int(now.Month())
+	nowYear := strconv.Itoa(now.Year())
+
+	icon, _ := walk.NewIconFromImageForDPI(getIcon(), 64)
+
+	if err := (MainWindow{
+		AssignTo: &mainWindow.MainWindow,
+		Title:    "Dochazka",
+		Icon:     icon,
+		Size:     Size{Width: 300, Height: 300},
+		Layout:   VBox{},
+		MenuItems: []MenuItem{
+			Menu{
+				Text: "&File",
+				Items: []MenuItem{
+					Action{
+						Text: "E&xit",
+						OnTriggered: func() {
+							err := mainWindow.Close()
+							if err != nil {
+								return
+							}
+						},
+					},
 				},
 			},
-			PushButton{
-				Text: "Generuj Dochazku",
-				OnClicked: func() {
-					genPdf()
+			Menu{
+				Text: "Help",
+				Items: []MenuItem{
+					Action{
+						Text: "About",
+						OnTriggered: func() {
+							walk.MsgBox(mainWindow, "About", "Developed by Jiri Tribula", walk.MsgBoxIconInformation)
+						},
+					},
 				},
 			},
 		},
-	}.Run()
+		Children: []Widget{
+			Composite{
+				Layout: Grid{Columns: 2},
+				Children: []Widget{
+					Label{
+						Text: "Rok:",
+					},
+					LineEdit{
+						AssignTo:  &year,
+						Text:      nowYear,
+						MaxLength: 4,
+					},
+					Label{
+						Text: "Měsíc:",
+					},
+					ComboBox{
+						AssignTo:      &monthName,
+						BindingMember: "Id",
+						DisplayMember: "Name",
+						Model:         monthNames(),
+						CurrentIndex:  nowMonth - 1,
+					},
+				},
+			},
+			Composite{
+				Layout: VBox{},
+				Children: []Widget{
+					PushButton{
+						Text:      "Generuj Dochazku",
+						Alignment: AlignHFarVCenter,
+						OnClicked: func() {
+							yearInt, _ := strconv.Atoi(year.Text())
+							genPdf(yearInt, monthName.CurrentIndex()+1, monthName.Text())
+							walk.MsgBox(mainWindow, "Done", "Hotovo: "+monthName.Text()+".pdf", walk.MsgBoxIconInformation)
+						},
+					},
+				},
+			},
+		},
+	}.Create()); err != nil {
+	}
+
+	mainWindow.Run()
 }
--- gen_pdf.go
+++ gen_pdf.go
@@ -1,45 +1,122 @@
 package main
 
 import (
+	"bytes"
+	"embed"
 	"github.com/signintech/gopdf"
-	"log"
+	"io"
+	"strconv"
 )
 
-func genPdf() {
+//go:embed times.ttf
+var timesFont embed.FS
+
+func getTimesFont() io.Reader {
+	dataBytes, _ := timesFont.ReadFile("times.ttf")
+	dataReader := bytes.NewReader(dataBytes)
+	return dataReader
+}
+
+//go:embed timesBold.ttf
+var timesBoldFont embed.FS
+
+func getTimesBoldFont() io.Reader {
+	dataBytes, _ := timesBoldFont.ReadFile("timesBold.ttf")
+	dataReader := bytes.NewReader(dataBytes)
+	return dataReader
+}
+
+func genPdf(year int, monthIndex int, monthName string) {
 	pdf := gopdf.GoPdf{}
 	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4}) //595.28, 841.89 = A4
 	pdf.AddPage()
-	err := pdf.AddTTFFont("times", "times.ttf")
+	err := pdf.AddTTFFontByReader("times", getTimesFont())
 	if err != nil {
-		log.Print(err.Error())
 		return
 	}
-	err = pdf.SetFont("times", "", 14)
-	if err != nil {
-		log.Print(err.Error())
-		return
-	}
-	pdf.SetLineWidth(0.1)
-	pdf.SetFillColor(255, 255, 255) //setup fill color
-	pdf.RectFromUpperLeftWithStyle(10, 10, 500, 100, "FD")
-	pdf.SetFillColor(0, 0, 0)
-
-	text := "Text"
-	textw, _ := pdf.MeasureTextWidth(text)
-	x := 10 + (500 / 2) - (textw / 2)
-
-	pdf.SetX(x)
-
-	y := 10 + (100 / 2) - (float64(14) / 2)
-
-	pdf.SetY(y)
-	err = pdf.Cell(nil, "")
+	err = pdf.AddTTFFontByReader("timesBold", getTimesBoldFont())
 	if err != nil {
 		return
 	}
 
-	err = pdf.WritePdf("test.pdf")
+	err = pdf.SetFont("timesBold", "", 16)
 	if err != nil {
 		return
 	}
+
+	pdf.SetX(20)
+	pdf.SetY(30)
+
+	err = pdf.Cell(nil, "FN Brno")
+	if err != nil {
+		return
+	}
+
+	err = pdf.SetFont("times", "", 16)
+	if err != nil {
+		return
+	}
+
+	pdf.SetX(20)
+	pdf.SetY(50)
+
+	err = pdf.Cell(nil, "Výkaz mzdových nároků za měsíc ")
+	if err != nil {
+		return
+	}
+
+	err = pdf.SetFont("timesBold", "", 16)
+	if err != nil {
+		return
+	}
+	pdf.SetX(244)
+	err = pdf.Cell(nil, monthName+" "+strconv.Itoa(year))
+	if err != nil {
+		return
+	}
+
+	err = pdf.SetFont("timesBold", "", 12)
+	if err != nil {
+		return
+	}
+	pdf.SetX(420)
+	pdf.SetY(54)
+	err = pdf.Cell(nil, "NS:")
+	if err != nil {
+		return
+	}
+
+	pdf.SetX(420)
+	pdf.SetY(90)
+	err = pdf.Cell(nil, "Osobní č:")
+	if err != nil {
+		return
+	}
+
+	pdf.SetLineWidth(2)
+	pdf.SetLineType("")
+	pdf.Line(20, 120, 360, 120)
+
+	err = pdf.SetFont("times", "", 10)
+	if err != nil {
+		return
+	}
+
+	pdf.SetX(50)
+	pdf.SetY(125)
+	err = pdf.Cell(nil, "prijmeni                          jmeno                                 titul                        funkce")
+	if err != nil {
+		return
+	}
+
+	err = pdf.WritePdf(monthName + ".pdf")
+	if err != nil {
+		return
+	}
+
+}
+
+func main() {
+
+	genPdf(2021, 4, "Duben")
 }
